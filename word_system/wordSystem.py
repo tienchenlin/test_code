52b036ac69e54532c016cf1627ec9f465cebf24d
tienchenlin <likeredbean@hotmail.com>
2018-09-06 08:08:05 +0800

    EN_word.txt

---
+++ word_system/wordSystem.py
@@ -0,0 +1,87 @@
+###功能函數###
+
+def mune():
+    os.system("cls")
+    print("單字卡系統")
+    print("==================")
+    print("1. 新增單字")
+    print("2. 全部內容")
+    print("3. 修改翻譯")
+    print("0. 結束系統")
+    print("==================")
+
+def readData():
+    with open("EN_word.txt", "r", encoding = "UTF-8-sig") as f:
+        wordData = f.read()
+        if wordData != "":
+            data = ast.literal_eval(wordData)
+            return data
+        else:
+            return dict()
+
+def allData():
+    print(f"單字\t翻譯")
+    print("==================")
+    for row in data:
+        print(f"{row:10}{data[row]:10}")
+    print("==================")
+    input("按任意鍵返回主選單")
+
+def inputData():
+    while True:
+        newWord = input("請輸入新單字or按Enter返回主選單:\n")
+        if newWord == "":
+            break
+        elif newWord in data:
+            print(f"{newWord} 單字已重複")
+            continue
+        translation = input("請輸入翻譯:\n")
+        data[newWord] = translation
+        with open("EN_word.txt", "w",encoding = "UTF-8-sig") as f:
+            f.write(str(data))
+        print("單字已儲存")
+
+
+def editData():
+    while True:
+        editWord = input("請輸入想修改的單字or按Enter返回主選單: \n單字: ")
+        if editWord == "": break
+        elif editWord not in data:
+            print(f"{editWord} 單字不存在")    
+        elif editWord in data:
+            print("舊翻譯:", data[editWord])
+            edtrans = input("請輸入修改翻譯:\n")
+            if edtrans == "":
+                print("請重新輸入單字")
+                continue
+            data[editWord] = edtrans
+            with open("EN_word.txt", "w",encoding = "UTF-8-sig") as f:
+                f.write(str(data))
+            print("單字翻譯已修改")
+
+
+    
+
+###主程式###
+
+import os,ast
+
+data = dict()
+
+data = readData()
+
+while True:
+    mune()
+
+    option = int(input("請輸入你的選擇: "))
+
+    if option == 1:
+        inputData()
+    elif option == 2:
+        allData()
+    elif option == 3:
+        editData()
+    else:
+        break
+
+print("程式已關閉") 
