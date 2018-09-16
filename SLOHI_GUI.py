8d9a72a74f95d65be835acbe57d3665e01020368
tienchenlin <likeredbean@hotmail.com>
2018-09-16 15:55:25 +0800

    hwp2

--- SLOHI_GUI.py
+++ SLOHI_GUI.py
@@ -1,16 +1,16 @@
 def slohi():
     tables = pandas.read_html("http://www.stockq.org/market/commodity.php")
-    table = tables[7]
-    print(table)
-  
+    table = tables[7] 
     df1 =table.drop(table.index[[0,1]])
     df1.columns = ["商品","買價","漲跌","比例","台北"]
     df3 = df1["商品"].values
     df1.index =[ i for i in df3]
-    df1.drop(["商品"], axis = 1)
-    table.values
+    df2 = df1.drop(["商品"], axis = 1)
+    #print(df2)
+    msgtext.set(df2)    
     
-    listtext.insert("end", tables)
+    #listtext.insert("end", )
+
 
 
 def win_close():
@@ -21,14 +21,14 @@ import tkinter
 
 ###主視窗
 win = tkinter.Tk()
-win.geometry("1050x768")
+win.geometry("800x600")
 win.title("原物料行情")
-win.resizable(False,False)
 ###其他視窗
 frame1 = tkinter.Frame(win)
 frame2 = tkinter.Frame(win)
 ###變數容器
-mydata = tkinter.StringVar
+mydata = tkinter.StringVar()
+msgtext = tkinter.StringVar()
 ###元件參數
 btn0 = tkinter.Button(frame1, text = "取得資料",width = 20, command = slohi)
 btn1 = tkinter.Button(frame1, text = "全部",width = 20)
@@ -37,9 +37,12 @@ btn3 = tkinter.Button(frame1, text = "買價",width = 20)
 btn4 = tkinter.Button(frame1, text = "漲跌",width = 20)
 btn5 = tkinter.Button(frame1, text = "比例",width = 20)
 
+msg = tkinter.Message(frame2,textvariable = msgtext,font= 40)
+msgtext.set("取得資料")
+
 close_btn = tkinter.Button(frame1,text="結束程式",width = 20,command=win_close)
 
-listtext = tkinter.Listbox(frame2, listvariable=mydata, width = 148,height = 100)
+#listtext = tkinter.Listbox(frame2, listvariable=mydata, width = 148,height = 100)
 
 ###介面配置
 frame1.grid(row = 0, column = 0)
@@ -52,6 +55,7 @@ btn5.grid(row = 0, column = 5,sticky ="en")
 
 close_btn.grid(row = 0, column = 6,sticky ="en")
 frame2.grid(row = 1, column = 0)
-listtext.grid()
+msg.grid()
+#listtext.grid()
 
 win.mainloop()
